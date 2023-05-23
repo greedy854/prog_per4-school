6a4b678230e00baa35f316a945e3ca57856299eb
sem <34904@ma-web.nl>
2023-05-23 16:55:27 +0200

    check

--- modal.js
+++ modal.js
@@ -16,20 +16,3 @@ closeBlueModal.onclick = function () {
 }
 }
 
-const white = document.getElementById("js--white");
-const valorant1 = document.getElementsByClassName("product");
-
-white.onclick = function () {
-    for (let i = 0; i < valorant1.length; i++) {
-        valorant1[i].style.display = "none";
-    }
-    document.getElementById("js--whiteModal").style.display = "grid";
-}
-
-const closewhiteModal = document.getElementById("js--closewhite");
-closewhiteModal.onclick = function () {
-    document.getElementById("js--whiteModal").style.display = "none";
-    for (let i = 0; i < valorant1.length; i++) {
-        valorant1[i].style.display = "block";
-}
-}
